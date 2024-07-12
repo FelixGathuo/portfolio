3517ce01ed797d96fb689baf844cfad2a0515a9b
FelixGathuo <gathuofelix@gmail.com>
2024-07-12 15:46:13 +0300

    Portfolio website

---
+++ components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import Image from 'next/image';
+import profile from '@/public/images/profile.jpg'
+
+const Sidebar = () => {
+  return (
+    <nav className="w-64 h-screen bg-gray-800 text-white flex flex-col">
+        {/* <Image src={profile} className='w-24 h-24 rounded-full'/> */}
+        <div className="flex items-center justify-center h-20 border-b border-gray-700">  
+            <h1 className="text-2xl font-bold">Felix Gathuo</h1>
+        </div>
+        <ul className="flex flex-col p-4">
+            <li className="mb-2">
+                <a href="#home" className="block py-2 px-4 rounded hover:bg-gray-700">Home</a>
+            </li>
+            <li className="mb-2">
+                <a href="#resume" className="block py-2 px-4 rounded hover:bg-gray-700">Resume</a>
+            </li>
+            <li className="mb-2">
+                <a href="#contact" className="block py-2 px-4 rounded hover:bg-gray-700">Contact</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Sidebar

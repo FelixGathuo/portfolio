15c20b4b70255cc67b257e89a996e5e9fbc72f8d
FelixGathuo <gathuofelix@gmail.com>
2024-07-12 17:12:55 +0300

    updated

--- app/page.js
+++ app/page.js
@@ -1,20 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import profile from "@/public/images/profile.jpg";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="flex-1 p-10">
-      <section id="home" className="center">
+    <div className="flex-1 p-10 ml-64">
+      <section id="home" className="text-white">
         <h2 className="text-4xl font-bold mb-4">Home</h2>
         <p>
           Welcome to my portfolio website. Here you can find my resume and
           contact information.
         </p>
+
+        
+        <i class="fa-brands fa-square-github"></i>
+        {/* <FontAwesomeIcon icon="fa-brands fa-square-github" /> */}
       </section>
       <section id="skills" className="mt-10">
         <h2 className="text-4xl font-bold mb-4 border-b">Skills</h2>
-        <p>
+        <div>
           <li>
             Full-stack web application development using HTML, CSS, JavaScript.
           </li>
@@ -27,15 +32,37 @@ const Home = () => {
           </li>
           <li>Test-driven development using Python.</li>
           <li>SQL and database modeling.</li>
-        </p>
+        </div>
       </section>
       <section id="resume" className="mt-10">
         <h2 className="text-4xl font-bold mb-4 border-b">Resume</h2>
-        <p>Here is my resume.</p>
+        <p>Software Engineer with 3+ years of experience in building systems</p>
+        <h2 className="text-2xl">Education</h2>
+        <p>
+          <i>BACHELOR OF SCIENCE - MATHEMATICS AND COMPUTER SCIENCE</i>
+          <br />
+          2015-2019<br/>
+          <i>Kenyatta University</i>
+        </p>
       </section>
       <section id="contact" className="mt-10">
         <h2 className="text-4xl font-bold mb-4 border-b">Contact</h2>
-        <p>Contact me at email@example.com.</p>
+        <div className="col-lg-6 d-flex align-items-stretch">
+          <p className="text-2xl">Email</p>
+          <Link href="emailt0:gathuofelix@gmail.com">
+            gathuofelix@gmail.com
+          </Link>
+          <h2 className="text-2xl">Github</h2>
+          <Link href="https://github.com/FelixGathuo">FelixGathuo</Link>
+          <i className="fa-brands fa-linkedin"></i>
+          <p>LinkedIn</p>
+          <Link href="https://www.linkedin.com/in/felixgathuo">
+            Felix Gathuo
+          </Link>
+          <i className="fa-brands fa-x-twitter"></i>
+          <p className="text-2xl">X</p>
+          <Link href="https://x.com/masterchief_F">@masterchief_F</Link>
+        </div>
       </section>
     </div>
   );
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -6,9 +6,9 @@ import profile from '@/public/images/profile.jpg'
 
 const Sidebar = () => {
   return (
-    <nav className="w-64 h-screen bg-gray-800 text-white flex flex-col">
-        {/* <Image src={profile} className='w-24 h-24 rounded-full'/> */}
-        <div className="flex items-center justify-center h-20 border-b border-gray-700">  
+    <nav className="fixed w-64 h-screen bg-gray-800 text-white flex flex-col">
+        <div className="flex items-center justify-center h-40 border-b border-gray-700">  
+            <Image src={profile} className='w-24 h-24 rounded-full'/>
             <h1 className="text-2xl font-bold">Felix Gathuo</h1>
         </div>
         <ul className="flex flex-col p-4">
